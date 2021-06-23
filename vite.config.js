04b1b6f9a47034c507837ba8b424d58c87343ff9
VilhelmNielsen <seb.vilhelm@gmail.com>
2021-06-23 14:56:18 +0200

    Add TypeScript

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("vite");
-const reactRefresh = require("@vitejs/plugin-react-refresh");
-
-module.exports = defineConfig({
-  build: {
-    outDir: "build",
-  },
-  plugins: [reactRefresh()],
-});
